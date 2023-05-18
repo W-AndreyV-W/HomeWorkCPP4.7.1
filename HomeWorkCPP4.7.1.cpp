2a7823133e5867f33242c53801fed11d77b27c18
Andrey V <Andreyxvv@hotmail.com>
2023-05-19 00:03:57 +0500

    first commit

---
+++ HomeWorkCPP4.7.1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <algorithm>
+#include <vector>
+
+int main()
+{
+    std::vector<int> arr{ 1,1,2,5,6,1,2,4 };
+
+    std::cout << "[IN]:\t";
+    for (auto i = arr.begin(); i < arr.end(); i++) {
+        std::cout << *i << " ";
+    }
+    std::cout << std::endl;
+
+    std::sort(arr.begin(), arr.end());
+    auto inter = std::unique(arr.begin(), arr.end());
+    arr.erase(inter, arr.end());
+
+    std::cout << "[OUT]:\t";
+    for (auto i = arr.begin(); i < arr.end(); i++) {
+        std::cout << *i << " ";
+    }
+    std::cout << std::endl;
+}
